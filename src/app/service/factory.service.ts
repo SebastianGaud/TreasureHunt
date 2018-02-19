2c5adb7212185981ee51f3cc00432589eb852bf7
Sebastiano <sebastiano.gaudeano@live.com>
2018-02-19 22:00:33 +0100

    Add new enviroment variables and add milestoneservice to factory method

--- src/app/service/factory.service.ts
+++ src/app/service/factory.service.ts
@@ -1,6 +1,17 @@
 import { Injectable } from '@angular/core';
+import { MilestoneService } from './milestone/milestone.service';
+import { FakeMilestoneService } from './milestone/fake-milestone.service';
+import { IMilestoneService } from './milestone/milestone-service';
+import { environment } from '../../environments/environment';
 
 @Injectable()
 export class FactoryService {
-  constructor() { }
+  constructor(
+    private milestoneService: MilestoneService,
+    private fakeMilestoneService: FakeMilestoneService
+  ) { }
+
+  getMilestoneService() : IMilestoneService {
+    return environment.useFakeProviders ? this.fakeMilestoneService : this.milestoneService;
+  }
 }
