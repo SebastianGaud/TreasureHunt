9193b63c775cb8985b510e12d092ac476d6a1ef8
Sebastiano <sebastiano.gaudeano@live.com>
2018-02-19 22:47:09 +0100

    Import observables

--- src/app/service/milestone/fake-milestone.service.ts
+++ src/app/service/milestone/fake-milestone.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { IMilestoneService } from './milestone-service';
 import { IMilestone } from '../../model/milestone/milestone.d';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class FakeMilestoneService implements IMilestoneService{
