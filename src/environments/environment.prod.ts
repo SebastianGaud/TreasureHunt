b3e02b9f7ac3a6cc57ad8247ca3c71bd65b2b7fb
Sebastiano <sebastiano.gaudeano@live.com>
2018-02-20 23:00:32 +0100

    Update travis file and use fake providers

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  useFakeProviders: false,
+  useFakeProviders: true,
   firebase : {
     apiKey: "AIzaSyAV9PlL4TgFFlpp3AzRow7ckspeAY4BQ5M",
     authDomain: "cacciaaltesoroprod.firebaseapp.com",
