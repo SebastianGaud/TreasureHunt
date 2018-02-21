f24d0240039677e6e80a161213d3c3b69577dcfc
Sebastiano <sebastiano.gaudeano@live.com>
2018-02-21 22:52:41 +0100

    Add milestone details components

---
+++ src/app/milestone-details/milestone-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'milestone-details',
+  templateUrl: './milestone-details.component.html',
+  styles: []
+})
+export class MilestoneDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
