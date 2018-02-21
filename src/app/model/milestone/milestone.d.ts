aba0e90c9bccba54517248c2a371949cc5862a94
Sebastiano <sebastiano.gaudeano@live.com>
2018-02-21 22:42:00 +0100

    Change milestone implementation

--- src/app/model/milestone/milestone.d.ts
+++ src/app/model/milestone/milestone.d.ts
@@ -1,4 +1,5 @@
 export interface IMilestone {
+	id: string;
 	opened: boolean;
 	name: string;
 	question: string;
--- src/app/model/milestone/milestone.ts
+++ src/app/model/milestone/milestone.ts
@@ -1,6 +1,7 @@
 import { IMilestone } from './milestone.d';
 
 export class Milestone implements IMilestone {
+	id: string;
 	opened: boolean;
 	name: string;
 	question: string;
