6f591ba6d4d53b767fe43c92fb35a0cab475c831
Sebastiano <sebastiano.gaudeano@live.com>
2018-02-24 17:33:58 +0100

    Change icon to milestone list

--- src/app/frontend/milestone-list-item/milestone-list-item.component.ts
+++ src/app/frontend/milestone-list-item/milestone-list-item.component.ts
@@ -1,16 +1,32 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, OnDestroy } from "@angular/core";
+import { FactoryService } from "../../service/factory.service";
+import { IMilestone } from "../../model/milestone/milestone.d";
+import { Subscription } from "rxjs/Subscription";
+import { Milestone } from "../../model/milestone/milestone";
 
 @Component({
   selector: "milestone-list-item",
   templateUrl: "./milestone-list-item.component.html",
   styles: []
 })
-export class MilestoneListItemComponent {
+export class MilestoneListItemComponent implements OnDestroy{
 
+  milestone: IMilestone = new Milestone;
   tappa: string = "TAPPA";
+  milestoneSubscription: Subscription;
   @Input("number") num: number;
   @Input("id")id :string;
 
-  constructor() { }
+  constructor(
+    private serviceFactory: FactoryService
+  ) { 
+    this.milestoneSubscription = serviceFactory.getMilestoneService().getMilestone(this.id).subscribe(s => {
+      this.milestone = s;
+    })
+  }
 
+
+  ngOnDestroy(): void {
+    this.milestoneSubscription.unsubscribe();
+  }
 }
