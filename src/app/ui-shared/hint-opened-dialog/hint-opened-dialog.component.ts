7c5fa425fe75eddc409c84c092a76ace83988bb5
Sebastiano <sebastiano.gaudeano@live.com>
2018-02-24 14:57:00 +0100

    Add opened hint dialog

---
+++ src/app/ui-shared/hint-opened-dialog/hint-opened-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'hint-opened-dialog',
+  templateUrl: './hint-opened-dialog.component.html',
+  styles: []
+})
+export class HintOpenedDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
