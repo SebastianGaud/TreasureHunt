4e40f155b7a0c5fd234cd43eb1796bc6ba963fc5
Sebastiano <sebastiano.gaudeano@live.com>
2018-03-02 11:55:04 +0100

    Add MilestoneAction

---
+++ src/app/actions/milestone.actions.ts
@@ -0,0 +1,22 @@
+import { IMilestone } from "../model/milestone/milestone.d";
+
+export const LOAD_MILESTONES = "LOAD_MILESTONES";
+export const LOAD_MILESTONES_SUCCESS = "LOAD_MILESTONES_SUCCESS";
+
+export class LoadMilestonesAction {
+	readonly type = LOAD_MILESTONES;
+	
+	constructor() {
+	}
+}
+
+export class LoadMilestonesSuccessAction {
+	readonly type = LOAD_MILESTONES_SUCCESS;
+	
+	constructor(public payload: IMilestone) {
+	}
+}
+
+export type Action
+	= LoadMilestonesAction
+	| LoadMilestonesSuccessAction
