176da9ab92c80dda94404b199d87923e06dd54c1
Sebastiano <sebastiano.gaudeano@live.com>
2018-03-04 15:50:35 +0100

    Add routing for generate milestone component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import { MilestoneDetailsComponent } from "./frontend/milestone-details/mileston
 import { FrontendEntryComponent } from "./frontend/frontend-entry/frontend-entry.component";
 import { BackendEntryComponent } from "./backend/backend-entry/backend-entry.component";
 import { OpenedMilestoneGuard } from "./guard/opened-milestone.guard";
+import { GenerateMilestoneComponent } from "./backend/generate-milestone/generate-milestone.component";
 
 const routes: Routes = [
   { path: "frontend", component: FrontendEntryComponent },
   { path: "backend", component: BackendEntryComponent },
+  { path: "generate-milestone", component: GenerateMilestoneComponent },
   { path: "milestone-details/:id", component: MilestoneDetailsComponent, canActivate: [OpenedMilestoneGuard] },
   { path: "", redirectTo: "frontend", pathMatch: "full" }
 ];
