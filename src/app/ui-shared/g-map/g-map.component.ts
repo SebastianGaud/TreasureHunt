ad0329ac5cec650688199592c40d427056cc4438
Sebastiano <sebastiano.gaudeano@live.com>
2018-03-07 11:56:35 +0100

    Add method to get coords

--- src/app/ui-shared/g-map/g-map.component.ts
+++ src/app/ui-shared/g-map/g-map.component.ts
@@ -10,32 +10,75 @@ import { } from '@types/googlemaps';
 export class GMapComponent implements OnInit {
 
   @ViewChild('gmap') gmapElement: any;
+
+
   map: google.maps.Map;
   autocomplete: google.maps.places.Autocomplete;
+  marker: google.maps.Marker;
+  geocoder: google.maps.Geocoder;
 
-  
-  constructor() { }
+
+  constructor() {
+    this.geocoder = new google.maps.Geocoder();
+   }
 
   ngOnInit() {
+    let initialLocation = new google.maps.LatLng(44.2227398, 12.040731199999982);
+
     var mapProp = {
-      center: new google.maps.LatLng(44.2227398, 12.040731199999982),
+      center: initialLocation,
       zoom: 15,
       mapTypeId: google.maps.MapTypeId.ROADMAP
     };
+
     this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
-  }
 
-  placing() : void {
-    google.maps.event.addListener(this.autocomplete, 'place_changed', ()=> {
-      let placeResult = this.autocomplete.getPlace();
-      console.log(placeResult);
+    this.marker = new google.maps.Marker({
+      position: initialLocation,
+      map: this.map,
+      visible: false
+    });
 
-      this.map.setCenter(placeResult.geometry.location);
+    google.maps.event.addListener(this.map, "click", (event) => {
+      this.addMarker(event.latLAng);
     });
   }
 
-  bindAutofocus(place) : void {
+  placing(): void {
+    google.maps.event.addListener(this.autocomplete, 'place_changed', () => {
+      let placeResult = this.autocomplete.getPlace();
+      if (placeResult !== null) {
+        this.map.setCenter(placeResult.geometry.location);
+        this.addMarker(placeResult.geometry.location);
+      }
+    });
+  }
+
+  bindAutofocus(place): void {
     this.autocomplete = new google.maps.places.Autocomplete(place, {});
+  }
+
+  private addMarker(latLAng: google.maps.LatLng){
+    this.marker.setVisible(true);
+    this.marker.setPosition(latLAng);
+    this.marker.setMap(this.map);
+    this.map.panTo(latLAng);
+  }
+
+  private codeLatLng(coord: google.maps.LatLng) : google.maps.GeocoderResult | void{
+    this.geocoder.geocode({
+      location: coord
+    }, function (results, status) {
+      if (status === google.maps.GeocoderStatus.OK) {
+        if (results[1]) {
+          return results[1];
+        } else {
+          alert('No results found');
+        }
+      } else {
+        alert('Geocoder failed due to: ' + status);
+      }
+    });
   }
 
 }
