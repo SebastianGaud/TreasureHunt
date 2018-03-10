65c738142c0420b9f78266868745a044f269ac62
Sebastiano <sebastiano.gaudeano@live.com>
2018-03-10 10:40:09 +0100

    Add component to Generate teams

---
+++ src/app/backend/generate-team/generate-team.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'generate-team',
+  templateUrl: './generate-team.component.html',
+  styles: []
+})
+export class GenerateTeamComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
