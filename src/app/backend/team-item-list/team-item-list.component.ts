1280d136c850737b046cd5f7b28568b530107736
Sebastiano <sebastiano.gaudeano@live.com>
2018-03-10 12:44:19 +0100

    Add component for displaying teams

---
+++ src/app/backend/team-item-list/team-item-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'team-item-list',
+  templateUrl: './team-item-list.component.html',
+  styles: []
+})
+export class TeamItemListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
