b71fd372e374056771e638cd7d232280ff88aa3e
Sebastiano <sebastiano.gaudeano@live.com>
2018-03-12 11:29:02 +0100

    Add sortable for ordering milestones

--- src/app/backend/team-details/team-details.component.ts
+++ src/app/backend/team-details/team-details.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TeamDetailsComponent implements OnInit {
 
+  indices = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public orderChanged($event) {
+    console.log($event);
   }
 
 }
