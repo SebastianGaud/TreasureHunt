52b89904bcc121ad06de74f210aaacc69829fe25
Sebastiano <sebastiano.gaudeano@live.com>
2018-03-13 11:42:24 +0100

    Add guard for register teams

--- src/app/guard/frontend-team-guard.guard.ts
+++ src/app/guard/frontend-team-guard.guard.ts
@@ -1,12 +1,20 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
+import { AppState } from '../model/app-state';
+import { Store } from '@ngrx/store';
 
 @Injectable()
 export class FrontendTeamGuardGuard implements CanActivate {
+  constructor(
+    private store: Store<AppState>
+  ) { }
+
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    return true;
+    return this.store.select(state => state.gameteam.token).map(s => {
+      return s !== null;
+    });
   }
 }
--- src/app/model/game/game-team.d.ts
+++ src/app/model/game/game-team.d.ts
@@ -2,6 +2,7 @@ import { FirebaseMilestone } from "../firebase/firebase-milestone";
 import { FirebaseTeam } from "../firebase/firebase-team";
 
 export interface IMilestonesTeam {
+	token: boolean;
 	key: string;
 	milestones: Array<FirebaseMilestone>
 }
