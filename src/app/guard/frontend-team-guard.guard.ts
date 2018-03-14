812551b224f586480b9e4822de5aa27054f95fc4
Sebastiano <sebastiano.gaudeano@live.com>
2018-03-14 20:37:59 +0100

    different implementation for guard

--- src/app/guard/frontend-team-guard.guard.ts
+++ src/app/guard/frontend-team-guard.guard.ts
@@ -3,23 +3,23 @@ import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from
 import { Observable } from 'rxjs/Observable';
 import { AppState } from '../model/app-state';
 import { Store } from '@ngrx/store';
+import { CookieService } from '../service/cookie-service.service';
 
 @Injectable()
 export class FrontendTeamGuard implements CanActivate {
   constructor(
     private store: Store<AppState>,
-    private router: Router
+    private router: Router,
+    private cookieService: CookieService
   ) { }
 
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    return this.store.select(state => state.gameteam).map(s => {
-      if (s != null){
-        return true;
-      }
-      this.router.navigate(['/team-wizard']);
-      return false;
-    });
+    if (this.cookieService.read('Authentication') != null) {
+      return true;
+    }
+    this.router.navigate(['/team-wizard']);
+    return false;
   }
 }
