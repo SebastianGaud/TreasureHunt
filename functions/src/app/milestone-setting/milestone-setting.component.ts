77f6783ad661639f5527af6541281a49f2004c7c
Sebastiano <sebastiano.gaudeano@live.com>
2018-03-22 22:32:50 +0100

    Add component for setting the milestones

---
+++ functions/src/app/milestone-setting/milestone-setting.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'milestone-setting',
+  templateUrl: './milestone-setting.component.html',
+  styles: []
+})
+export class MilestoneSettingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
