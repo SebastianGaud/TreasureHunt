86166d1b89df6cc820ea4562690655318f720d85
Sebastiano <sebastiano.gaudeano@live.com>
2018-03-22 22:41:17 +0100

    Add component to manage milestones

--- src/app/backend/milestone-setting/milestone-setting.component.ts
+++ src/app/backend/milestone-setting/milestone-setting.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import * as MilestoneActions from '../../actions/milestone.actions';
+import { AppState } from '../../model/app-state';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs/Observable';
+import { FirebaseMilestone } from '../../model/firebase/firebase-milestone';
 
 @Component({
   selector: 'milestone-setting',
   templateUrl: './milestone-setting.component.html',
   styles: []
 })
-export class MilestoneSettingComponent implements OnInit {
+export class MilestoneSettingComponent implements OnDestroy {
 
-  constructor() { }
+  milestones$: Observable<FirebaseMilestone[]>;
 
-  ngOnInit() {
+  constructor(
+    private store: Store<AppState>
+  ) {
+    this.store.dispatch(new MilestoneActions.ConnectMilestoneAction());
+    this.milestones$ = this.store.select<FirebaseMilestone[]>(state => state.milestones);
   }
 
+  ngOnDestroy(): void {
+    this.store.dispatch(new MilestoneActions.DisconnectMilestonesAction());
+  }
 }
