7b91698e1188ff417e99ce7d7289dbeaacf88202
Sebastiano <sebastiano.gaudeano@live.com>
2018-03-31 11:38:52 +0200

    Close #30

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,19 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, NavigationStart } from '@angular/router';
+import { AngularFireAuth } from 'angularfire2/auth';
 
 @Component({
   selector: "app-root",
   templateUrl: "app.component.html",
   styles: [],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  constructor(
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.router.navigate(['/frontend']);   
+  }
 }
--- src/app/ui-shared/navbar/navbar.component.ts
+++ src/app/ui-shared/navbar/navbar.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { Router, NavigationStart } from "@angular/router";
 import { AngularFireAuth } from "angularfire2/auth";
 import { Observable } from "rxjs/Observable";
+import { User } from "@firebase/auth-types";
 
 @Component({
   selector: "navbar",
@@ -9,21 +10,16 @@ import { Observable } from "rxjs/Observable";
   styles: []
 })
 export class NavbarComponent {
-
-  href: string;
-  isLogged: boolean;
+  u: User;
 
   constructor(
-    private router: Router,
     private afAuth: AngularFireAuth
   ) { 
-    router.events.filter(event => event instanceof NavigationStart).first()
-    .subscribe((event:NavigationStart) => {
-      this.href = event.url;
-    });
 
     this.afAuth.authState.subscribe(u => {
-      this.isLogged = u && u.uid != null && !u.isAnonymous;
+      if (u) {
+        this.u = u;
+      }
     })
   }
 }
