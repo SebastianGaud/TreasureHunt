e08fa61c98fd27cfa7cfb678113a88ebf2ee3065
Sebastiano <sebastiano.gaudeano@live.com>
2018-05-06 16:58:14 +0200

    Implement Team log-out

--- src/app/ui-shared/navbar/navbar.component.ts
+++ src/app/ui-shared/navbar/navbar.component.ts
@@ -3,6 +3,13 @@ import { Router, NavigationStart } from "@angular/router";
 import { AngularFireAuth } from "angularfire2/auth";
 import { Observable } from "rxjs/Observable";
 import { User } from "@firebase/auth-types";
+import { CookieService } from "../../service/cookie-service.service";
+import { Consts } from "../../../environments/Consts";
+import { TeamService } from "../../service/team/team.service";
+import { AppState } from "../../model/app-state";
+import { Store } from "@ngrx/store";
+import { AngularFireDatabase } from "angularfire2/database";
+import { Team } from "../../model/team/team";
 
 @Component({
   selector: "navbar",
@@ -13,7 +20,11 @@ export class NavbarComponent {
   u: User;
 
   constructor(
-    private afAuth: AngularFireAuth
+    private afAuth: AngularFireAuth,
+    private cookieService: CookieService,
+    private store: Store<AppState>,
+    private db: AngularFireDatabase,
+    private router: Router
   ) { 
 
     this.afAuth.authState.subscribe(u => {
@@ -21,5 +32,22 @@ export class NavbarComponent {
         this.u = u;
       }
     })
+  }
+
+  public logOut() {
+    let token = this.cookieService.read(Consts.CookieAuth);    
+    this.store.select(state => state.gameTeam).first().subscribe(t => {
+      const itemRef =  this.db.object(`teams/${token}`);
+      itemRef.update({token: false}).then(() => {
+        this.makeSignOutOperations();
+      });
+    });
+  }
+
+  private makeSignOutOperations() {
+    this.afAuth.auth.signOut();
+    this.cookieService.remove(Consts.CookieAuth);
+    this.router.navigate(['team-wizard']);
+    this.u = undefined;
   }
 }
